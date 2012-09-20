8d67ec27a77b49a252f43331d239fc923cb5a657
Daniel Gelbaum <danielgelbaum@gmail.com>
2012-09-20 16:16:39 -0400

    Revert "Revert "Removed references to rollins.com""
    This reverts commit e64abdce75e293dbd617ed5b29eff52214ec0e85.

---
+++ VanMan.Importer/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace VanMan.Importer
+{
+    class Program
+    {
+        private static string VanitiesFile = "vanities.txt";
+        static void Main(string[] args)
+        {
+            if (File.Exists(VanitiesFile))
+            {
+                IEnumerable<string> vanities = File.ReadLines(VanitiesFile);
+                using (FileStream resultsFs = new FileStream("VanitiesToUpload.csv", FileMode.Create, FileAccess.Write))
+                {
+                    using (StreamWriter resultsSw = new StreamWriter(resultsFs))
+                    {
+                        foreach (var vanity in vanities)
+                        {
+                            // Sample input line
+                            // rollins.com,http://www.rollins.com
+                            var splitVanity = vanity.Split(',');
+                            if (splitVanity.Length == 2)
+                            {
+                                Uri source = new UriBuilder(splitVanity[0]).Uri;
+                                Uri destination = new UriBuilder(splitVanity[1]).Uri;
+                                string url = source.ToString();
+                                byte[] toEncodeAsBytes = System.Text.ASCIIEncoding.ASCII.GetBytes(url);
+                                int options = (string.IsNullOrWhiteSpace(destination.Query)) ? 3 : 0; // TODO: Parse the destination URL to determin options
+                                resultsSw.WriteLine(string.Format(",{0},{1},{2}", Convert.ToBase64String(toEncodeAsBytes), splitVanity[1], options));
+                            }
+                        }
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine(string.Format("File {0} not found. Aborting.", VanitiesFile));
+            }
+        }
+    }
+}
