df004a065e399fd696f77639d54fec3e5e48d0fb
Daniel Gelbaum <danielgelbaum@gmail.com>
2012-10-09 17:05:18 -0400

    Revert "Revert "Revert "Re-organized code for test project"""
    This reverts commit 8dd12dc16e2a69c59c184d8bf2ce989540ca9e44.

---
+++ VanMan.WebApplication/Models/SourceOptions.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VanMan.WebApplication.App_Code
+{
+    public enum SourceOptions
+    {
+        StartsWith,
+        Exact,
+    }
+}
